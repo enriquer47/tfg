a7de4fcba2b87420b1b9e7394dc2b51a4c16f8e0
Enrique Martín <enrique.martin.gutierrez@alumnos.upm.es>
2023-01-31 12:22:15 +0100

    inicio de sesión básico completado

--- app/src/main/java/com/example/tfg/Login.java
+++ app/src/main/java/com/example/tfg/Login.java
@@ -1,14 +1,100 @@
 package com.example.tfg;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.internal.InternalTokenProvider;
 
 public class Login extends AppCompatActivity {
 
+    TextInputEditText editTextEmail, editTextPassword;
+    Button buttonLogIn;
+    FirebaseAuth mAuth;
+    ProgressBar barraLogin;
+    TextView registerNow;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent= new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth= FirebaseAuth.getInstance();
+        editTextEmail= findViewById(R.id.email);
+        editTextPassword= findViewById(R.id.password);
+        buttonLogIn=findViewById(R.id.loginButton);
+        barraLogin=findViewById(R.id.progreso_login);
+        registerNow=findViewById(R.id.registerNow);
+
+        registerNow.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent= new Intent(getApplicationContext(), Register.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        buttonLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                barraLogin.setVisibility(View.VISIBLE);
+                String email,password;
+                email=String.valueOf(editTextEmail.getText());
+                password=String.valueOf(editTextPassword.getText().toString());
+
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(Login.this, "Debe introducir un email", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(Login.this, "Debe introducir una contraseña", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                barraLogin.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getApplicationContext(), "Inicio de sesión completado", Toast.LENGTH_LONG).show();
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+
+                                    Toast.makeText(Login.this, "Error al iniciar sesion", Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
--- app/src/main/java/com/example/tfg/Register.java
+++ app/src/main/java/com/example/tfg/Register.java
@@ -1,14 +1,104 @@
 package com.example.tfg;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
 
+    TextInputEditText editTextEmail, editTextPassword;
+    Button buttonReg;
+    FirebaseAuth mAuth;
+    ProgressBar barraRegistro;
+    TextView loginNow;
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent= new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        mAuth= FirebaseAuth.getInstance();
+        editTextEmail= findViewById(R.id.email);
+        editTextPassword= findViewById(R.id.password);
+        buttonReg=findViewById(R.id.registerButton);
+        barraRegistro=findViewById(R.id.progreso_registro);
+        loginNow=findViewById(R.id.loginNow);
+
+        loginNow.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent= new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                 finish();
+            }
+        });
+
+        buttonReg.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                barraRegistro.setVisibility(View.VISIBLE);
+                String email,password;
+                email=String.valueOf(editTextEmail.getText());
+                password=String.valueOf(editTextPassword.getText().toString());
+
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(Register.this, "Debe introducir un email", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(Register.this, "Debe introducir una contraseña", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                barraRegistro.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(Register.this, "Registro Completado",
+                                            Toast.LENGTH_SHORT).show();
+                                    Intent intent= new Intent(getApplicationContext(), Login.class);
+                                    startActivity(intent);
+                                    finish();
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(Register.this, "Error en el registro",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
