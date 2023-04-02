71ed5d8010de97fbe4362d151c9437246cc07121
cristian.bahamode <cristian.bahamonde.santamaria@gmail.com>
2023-04-02 20:07:50 +0200

    Implement UI-Auth

--- app/src/main/java/com/example/tfg/Login.java
+++ app/src/main/java/com/example/tfg/Login.java
@@ -1,29 +1,30 @@
 package com.example.tfg;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.Build;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.text.TextUtils;
+
 import android.util.Log;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.NumberPicker;
-import android.widget.PopupWindow;
+
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.tfg.Model.Usuario;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
@@ -36,17 +37,14 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class Login extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 
-    TextInputEditText editTextEmail, editTextPassword;
-    Button buttonLogIn;
-    ProgressBar barraLogin;
-    TextView registerNow;
-    ImageView info;
+public class Login extends AppCompatActivity {
 
     FirebaseAuth mAuth;
     DatabaseReference myRef;
-    AlertDialog infoDialogo;
     final String[] tipoCuentaArray={"Alumno", "Profesor", "Padre"};
 
 
@@ -55,7 +53,7 @@ public class Login extends AppCompatActivity {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        myRef= FirebaseDatabase.getInstance("https://registro-tfg-92125-default-rtdb.europe-west1.firebasedatabase.app").getReference();
+        myRef= FirebaseDatabase.getInstance("https://prueba-c426b-default-rtdb.europe-west1.firebasedatabase.app").getReference();
 
         getSupportActionBar().setTitle("Inicio de sesión");
 
@@ -91,94 +89,64 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
         mAuth= FirebaseAuth.getInstance();
-        editTextEmail= findViewById(R.id.email);
-        editTextPassword= findViewById(R.id.password);
-        buttonLogIn=findViewById(R.id.loginButton);
-        barraLogin=findViewById(R.id.progreso_login);
-        registerNow=findViewById(R.id.registerNow);
-        info=findViewById(R.id.info);
+        //Change lenguage to spanish
+        Locale locale = new Locale("es", "ES");
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
 
-        buildDialog();
-        registerNow.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Intent intent= new Intent(getApplicationContext(), Register.class);
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build()
+                );
+
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setLogo(R.drawable.logo)
+                .setTheme(R.style.LoginTheme)
+                .build();
+        signInLauncher.launch(signInIntent);
+
+    }
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            // Successfully signed in
+            if(response.isNewUser()){
+                Intent intent = new Intent(getApplicationContext(), Register.class);
+                startActivity(intent);
+                finish();
+            }else{
+                Intent intent = new Intent(getApplicationContext(), PrincipalAlumno.class);
                 startActivity(intent);
                 finish();
             }
-        });
+            //FirebaseUser currentUser = mAuth.getCurrentUser();
+            //myRef= FirebaseDatabase.getInstance("https://prueba-c426b-default-rtdb.europe-west1.firebasedatabase.app").getReference();
+            //Usuario usuario = new Usuario(currentUser.getEmail(), tipoCuentaArray[0]);
+           // myRef.child("usuarios").child(currentUser.getUid()).setValue(usuario);
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // handle the error.
+            if(response != null)
+                Log.i("Error", response.getError().getMessage());
+            else
+                Log.i("Error", "Error al iniciar sesion");
 
-        buttonLogIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                barraLogin.setVisibility(View.VISIBLE);
-                String email,password;
-                email=String.valueOf(editTextEmail.getText());
-                password=String.valueOf(editTextPassword.getText().toString());
-
-                if(TextUtils.isEmpty(email)){
-                    Toast.makeText(Login.this, "Debe introducir un email", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                if (TextUtils.isEmpty(password)) {
-                    Toast.makeText(Login.this, "Debe introducir una contraseña", Toast.LENGTH_LONG).show();
-                    return;
-                }
-
-                //Autenticación con Firebase
-                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener( new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                barraLogin.setVisibility(View.GONE);
-                                if (task.isSuccessful()) {
-                                    myRef.child("usuarios").child(task.getResult().getUser().getUid()).child("tipoCuenta").addListenerForSingleValueEvent(new ValueEventListener() {
-                                        @Override
-                                        public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                            if(snapshot.exists()){
-                                                Intent intent;
-                                                if(snapshot.getValue(String.class).equals(tipoCuentaArray[1]))
-                                                    intent = new Intent(getApplicationContext(), PrincipalProfesor.class);
-                                                else
-                                                if(snapshot.getValue(String.class).equals(tipoCuentaArray[0]))
-                                                    intent = new Intent(getApplicationContext(), PrincipalAlumno.class);
-                                                else
-                                                    intent =new Intent(getApplicationContext(), PrincipalPadre.class);
-                                                startActivity(intent);
-                                                finish();
-                                            }
-                                        }
-                                        @Override
-                                        public void onCancelled(@NonNull DatabaseError error) {
-
-                                        }
-                                    });
-
-                                } else {
-
-                                    Toast.makeText(Login.this, "Error al iniciar sesion", Toast.LENGTH_SHORT).show();
-
-                                }
-                            }
-                        });
-            }
-        });
-        info.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                infoDialogo.show();
-            }
-        });
+        }
     }
-    private void buildDialog() {
-        AlertDialog.Builder builder= new AlertDialog.Builder(this);
-        View view= getLayoutInflater().inflate(R.layout.infologin, null);
-
-        builder.setView(view);
-
-        infoDialogo= builder.create();
-
-    }
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                @Override
+                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                    onSignInResult(result);
+                }
+            }
+    );
 }
